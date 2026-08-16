4239cacb6c699ceb7006e1f07117e0e00039e8fe
pawanjoshi1926-creator <pawanjoshi1926@gmail.com>
2026-08-16 23:29:27 +0530

    Update recommendation endpoint to use new model
    Refactor movie recommendation endpoint to use new model prediction method.

--- movie_recommender/app/routers/recommendation.py
+++ movie_recommender/app/routers/recommendation.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 
-from app.dependencies.models import ModelStore, get_models
+from app.dependencies.model import  get_model
 from app.recommender import recommend
 from app.schemas import RecommendationResponse
 
@@ -10,28 +10,16 @@ router = APIRouter(
 )
 
 
-@router.get(
-    "/recommend",
-    response_model=RecommendationResponse
-)
+@router.get("/recommend")
 def recommend_movie(
     movie: str,
-    models: ModelStore = Depends(get_models)
+    model=Depends(get_model)
 ):
 
-    result = recommend(
-        movie_name=movie,
-        movies=models.movies,
-        similarity=models.similarity
+    result = model.predict(
+        {
+            "movie": [movie]
+        }
     )
-
-    if len(result["recommendations"]) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail={
-                "message": "Movie not found",
-                "suggestions": result["suggestions"]
-            }
-        )
 
     return result
